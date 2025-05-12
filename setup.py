04f4ed075ac4982f23b471f5e9f501c261d2dc94
YonierAlexisQuiceno <yonier.quiceno@est.iudigital.edu.co>
2025-05-12 00:17:53 +0000

    Creacion de setup con su documentacion y instalacion de dependencias

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="edu_bidata",
+    author="Yonier Alexis Quiceno",
+    author_email="yonier.quiceno@est.iudigital.edu.co",
+    description="",
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests",
+        "BeautifulSoup4"
+    ] 
+)
